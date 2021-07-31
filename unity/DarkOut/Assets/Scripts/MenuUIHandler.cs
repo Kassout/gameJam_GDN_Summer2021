33fb1b1b9a590a0093d068cf4d918c0ee16c77c0
mheliot <maxime.heliotpro@gmail.com>
2021-07-31 22:08:15 +0200

    Add player controller script

--- unity/DarkOut/Assets/Scripts/MenuUIHandler.cs
+++ unity/DarkOut/Assets/Scripts/MenuUIHandler.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Sets the script to be executed later than all default scripts
+// This is helpful for UI, since other things may need to be initialized before setting the UI
+[DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// This method is used to start the game from the main menu.
+    /// </summary>
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(2);
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    /// <summary>
+    /// This method is used to stop and close the game.
+    /// </summary>
+    public void Exit()
     {
-        
+        // pre-compilation tasks, if in unity editor compile code to exit play mode,
+        // else compile to quit the application.
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
--- unity/DarkOut/Assets/Scripts/PlayerController.cs
+++ unity/DarkOut/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,98 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
+/// <summary>
+/// Class <c>PlayerController</c> is a Unity component script used to manage the different player behaviours.
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// Instance variable <c>rigidBody</c> represents the player's rigidbody.
+    /// </summary>
+    public Rigidbody2D rigidBody;
+    
+    /// <summary>
+    /// Instance variable <c>moveSpeed</c> represents the player's movement speed.
+    /// </summary>
+    [SerializeField]
+    private float moveSpeed = 5f;
 
-    // Update is called once per frame
+    /// <summary>
+    /// Instance variable <c>groundTileMap</c> represents the ground tile map on which player will walk on.
+    /// </summary>
+    [SerializeField]
+    private Tilemap groundTileMap;
+
+    /// <summary>
+    /// Instance variable <c>collisionTileMap</c> represents the tile map containing the different obstacles the player could collide with.
+    /// </summary>
+    [SerializeField]
+    private Tilemap collisionTileMap;
+
+    /// <summary>
+    /// Instance variable <c>interactionTileMap</c> represents the tile map containing the different object the player could interact with.
+    /// </summary>
+    [SerializeField]
+    private Tilemap interactionTileMap;
+
+    /// <summary>
+    /// Instance variable <c>movement</c> represents the player's movement.
+    /// </summary>
+    private Vector2 _movement;
+
+    /// <summary>
+    /// This method is called once per frame
+    /// </summary>
     void Update()
     {
-        
+        // Input
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    /// <summary>
+    /// This method is called every fixed frame-rate frame.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        // Move
+        MovePlayer();
+    }
+    
+    /// <summary>
+    /// This method is used to move the player.
+    /// </summary>
+    private void MovePlayer()
+    {
+        Vector2 direction = rigidBody.position + _movement * (moveSpeed * Time.fixedDeltaTime);
+        if (CanMove(direction))
+        {
+            rigidBody.MovePosition(direction);
+        }
+    }
+
+    /// <summary>
+    /// This method is used to check if whether or not a player can move in the targeted direction considerate eventual obstacles.
+    /// </summary>
+    /// <param name="direction">A <c>Vector3</c> Unity structure representing the movement direction of the player.</param>
+    /// <returns>A boolean value representing the state of movement allowance.</returns>
+    private bool CanMove(Vector3 direction)
+    {
+        Vector3Int gridPosition = groundTileMap.WorldToCell(direction);
+        if (!groundTileMap.HasTile(gridPosition) || collisionTileMap.HasTile(gridPosition))
+        {
+            return false;
+        }
+
+        return true;
+    }
+    
+    // TODO : comments
+    private void Interact()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // TODO : do things on interact
+        }
     }
 }
