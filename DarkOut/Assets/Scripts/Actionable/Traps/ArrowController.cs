711d9c4793727addb34b147033a6d5791e79047d
mheliot <maxime.heliotpro@gmail.com>
2021-10-30 00:05:07 +0200

    Clean up ArrowController class

--- DarkOut/Assets/Scripts/Actionable/Traps/ArrowController.cs
+++ DarkOut/Assets/Scripts/Actionable/Traps/ArrowController.cs
@@ -6,50 +6,45 @@ using UnityEngine;
 /// </summary>
 public class ArrowController : MonoBehaviour
 {
+    #region Fields/Variables
+
     /// <summary>
     /// Instance variable <c>moveSpeed</c> represents the speed of the arrow.
     /// </summary>
-    [SerializeField]
-    private float moveSpeed = 10.0f;
+    [SerializeField] private float moveSpeed = 10.0f;
+    
+    /// <summary>
+    /// Instance variable <c>_direction</c> represents the direction vector of the arrow game object.
+    /// </summary>
+    private Vector2 _direction;
 
-    private Vector2 direction;
+    #endregion
+
+    #region MonoBehaviour
 
     /// <summary>
     /// This method is called once per frame
     /// </summary>
-    void Update()
+    private void Update()
     {
         MoveForward();
     }
-
-    public void SetDirection(Vector2 dir) {
-        direction = dir;
-        Rotate();
-    }
-
-    /// <summary>
-    /// This method is called to move the arrow.
-    /// </summary>
-    private void MoveForward()
-    {
-        transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
-    }
-
+    
     /// <summary>
     /// This method is called when another object enters a trigger collider attached to this object.
     /// </summary>
-    /// <param name="other">A <c>Collider2D</c> Unity component representing the collider of the object that it collides with.</param>
-    private void OnTriggerEnter2D(Collider2D other)
+    /// <param name="oCollider">A <c>Collider2D</c> Unity component representing the collider of the object that it collides with.</param>
+    private void OnTriggerEnter2D(Collider2D oCollider)
     {
-        if (other.CompareTag("Spring"))
+        if (oCollider.CompareTag("Spring"))
         {
-            transform.position = other.transform.position;
-            direction = other.GetComponent<SpringController>().GetDirection();
+            transform.position = oCollider.transform.position;
+            _direction = oCollider.GetComponent<SpringController>().GetDirection();
             Rotate();
-            transform.Translate(direction * 0.8f, Space.World);
-        } else if (other.CompareTag("InteractionObject"))
+            transform.Translate(_direction * 0.8f, Space.World);
+        } else if (oCollider.CompareTag("InteractionObject"))
         {
-            LeverController lever = other.GetComponent<LeverController>();
+            LeverController lever = oCollider.GetComponent<LeverController>();
             if(lever != null) {
                 IEnumerator coroutine = lever.PushSequenceOnInteraction();
                 lever.PassCoroutineRef(coroutine);
@@ -61,27 +56,57 @@ public class ArrowController : MonoBehaviour
     /// <summary>
     /// TODO: comments
     /// </summary>
-    /// <param name="other"></param>
-    private void OnCollisionEnter2D(Collision2D other)
+    /// <param name="oCollision"></param>
+    private void OnCollisionEnter2D(Collision2D oCollision)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (oCollision.gameObject.CompareTag("Player"))
         {
-            other.gameObject.GetComponent<PlayerController>().TakeDamage();
+            oCollision.gameObject.GetComponent<PlayerController>().TakeDamage();
         }
-        if(!other.gameObject.CompareTag("Spring") || !other.gameObject.CompareTag("InteractionObject")) {
+        if(!oCollision.gameObject.CompareTag("Spring") || !oCollision.gameObject.CompareTag("InteractionObject")) {
             Destroy(gameObject);
         }
     }
-       
+
+    #endregion
+
+    #region Private
+
+    /// <summary>
+    /// This method is called to move the arrow.
+    /// </summary>
+    private void MoveForward()
+    {
+        transform.Translate(_direction * (moveSpeed * Time.deltaTime), Space.World);
+    }
+
+    /// <summary>
+    /// This method is responsible for rotating the arrow according to the arrow object direction vector value.
+    /// </summary>
     private void Rotate() {
-        if(direction == Vector2.up) {
+        if(_direction == Vector2.up) {
             transform.rotation = Quaternion.Euler(0, 0, 90);
-        } else if (direction == Vector2.down) {
+        } else if (_direction == Vector2.down) {
             transform.rotation = Quaternion.Euler(0, 0, 270);
-        } else if (direction == Vector2.left) {
+        } else if (_direction == Vector2.left) {
             transform.rotation = Quaternion.Euler(0, 0, 0);
         } else {
             transform.rotation = Quaternion.Euler(0, 180, 0);
         }
     }
+
+    #endregion
+
+    #region Public
+
+    /// <summary>
+    /// This method is responsible for changing the arrow object direction vector value.
+    /// </summary>
+    /// <param name="dir">A <c>Vector2</c> Unity component representing the direction to setup the arrow.</param>
+    public void SetDirection(Vector2 dir) {
+        _direction = dir;
+        Rotate();
+    }
+
+    #endregion
 }
